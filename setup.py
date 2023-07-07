7c69143faf3eab431e0061a2841edaebafba4c7f
Marine Vovard <m.vovard@phytec.de>
2023-07-07 10:07:45 +0200

    🚚 Move files and add setup file to package

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kivydemo',
+    version='0.1.0',
+    description='Kivy demo for Phytec Products',
+    author='Marine Vovard',
+    author_email='m.vovard@phytec.de',
+    packages=find_packages(include=['kivydemo', 'kivydemo.*']),
+    install_requires=['kivy'],
+    entry_points={
+        'console_scripts': ['kivydemo=kivydemo.main:main']
+    },
+    package_data={'': ['*.kv', '*.jpg', '*.png']}
+)
