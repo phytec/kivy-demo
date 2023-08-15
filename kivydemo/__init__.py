f2b84a0c9afc788ae4110c6f8b2921c17a3b2c73
Marine Vovard <m.vovard@phytec.de>
2023-08-15 15:41:47 +0200

    🐛 Fix image path

--- kivydemo/__init__.py
+++ kivydemo/__init__.py
@@ -4,7 +4,8 @@ path_demo = os.path.dirname(__file__)
 """Path to DemoKivy package directory."""
 
 path_images = path_demo+'/images'
-if len(os.listdir(path_images)) <= 1:  # images folder empty except __init__ file
+if len(os.listdir(path_images)) <= 2:  # images folder empty except __init__ file and pycache
     path_images = '/usr/share/phytecvisuals'
 
-print("Path for my images", path_images)
+# Debug
+# print("Path for my images", path_images)
